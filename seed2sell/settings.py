b55385941d0ccb085b9488ec25edaab8f442d2af
Saiyed Misba <misbasaiyed20@gmail.com>
2026-08-11 18:46:58 +0530

    Fix static files for Vercel deployment

--- seed2sell/settings.py
+++ seed2sell/settings.py
@@ -145,12 +145,23 @@ USE_TZ = True
 # --------------------------------------------------
 # STATIC FILES
 # --------------------------------------------------
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 # --------------------------------------------------
 # MEDIA FILES
 # --------------------------------------------------
