93bfc28d945a517a20cdb9be830ecee92043ebce
Saiyed Misba <misbasaiyed20@gmail.com>
2026-08-11 09:34:21 +0530

    Fix production static files

--- seed2sell/settings.py
+++ seed2sell/settings.py
@@ -55,13 +55,14 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # --------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    ]
 
 
 # --------------------------------------------------
@@ -148,7 +149,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # --------------------------------------------------
 # MEDIA FILES
