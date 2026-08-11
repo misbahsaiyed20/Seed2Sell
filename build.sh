48f79a4972c28e5d4cfc629281df7a541b03c9ed
Saiyed Misba <misbasaiyed20@gmail.com>
2026-08-11 23:09:09 +0530

    Fix Vercel deployment configuration

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+mkdir -p static/media
+cp -r media/* static/media/
+
 python manage.py collectstatic --noinput
--- seed2sell/settings.py
+++ seed2sell/settings.py
@@ -168,7 +168,6 @@ STORAGES = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 # --------------------------------------------------
 # AUTH REDIRECTS
 # --------------------------------------------------
